414cd77435d4bb16fc046093e3ed6a37989b962d
Gl3Bbl4 <borisenko.gleb.work@gmail.com>
2019-07-03 14:41:13 +0400

    header v2

--- Front/project/src/components/Header.vue
+++ Front/project/src/components/Header.vue
@@ -1,23 +1,31 @@
 <template>
-    <header>
-        <div class="links-list">
-            <router-link to="/films" class="link">Фильмы</router-link>
-            <router-link to="/news" class="link">Новости</router-link>
-            <router-link to="/contacts" class="link">Контакты</router-link>
-        </div>
-    </header>
+    <div class="wrapper">
+        <div class="logo"></div>
+        <router-link to="/films" class="link">Фильмы</router-link>
+        <router-link to="/news" class="link">Новости</router-link>
+        <router-link to="/contacts" class="link">Контакты</router-link>
+        <button class="signIn">Войти</button>
+    </div>
 </template>
  
- <style scoped>
- header{
-    background-color: #F73;
-    width: 100%;
-    height: 240px;
- }
- .link{
-    text-decoration: none;
-    color: black;
-    margin: 4px;
- }
- </style>
- 
+<style lang="sass" scoped>
+$logo-width : 200px
+$logo-height : $logo-width
+.wrapper
+    background-color: #F73
+    padding: 70px
+    width: 100%
+    height: 240px
+    display: flex
+    justify-content: space-around
+    align-items: center
+    .logo
+        width: $logo-width
+        height: $logo-height
+        background-color: #F33
+    .link
+        text-decoration: none
+        color: black
+        font-size: 24px
+        font-weight: 100
+</style>
