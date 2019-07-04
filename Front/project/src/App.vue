3742c1a836b8990391b5810e20457be9925b33ee
maximelnui <52440685+maximelnui@users.noreply.github.com>
2019-07-04 13:12:19 +0400

    appa
    appa

--- Front/project/src/App.vue
+++ Front/project/src/App.vue
@@ -1,25 +1,16 @@
 <template>
   <div id="app">
-<<<<<<< HEAD
-<<<<<<< HEAD
     <div id="page" class="backgrount_color__main">
         <Header></Header>
         <Footer></Footer>
         <PageAuthorization></PageAuthorization>
     </div>
-=======
-=======
->>>>>>> 08fa605bb38d04ecea9f344c1bdfea80fcb66502
       <div class="wrapper-page">
         <div id="page">
             <Header></Header>
             <Footer></Footer>
         </div>
-      </div>
-<<<<<<< HEAD
->>>>>>> 08fa605bb38d04ecea9f344c1bdfea80fcb66502
-=======
->>>>>>> 08fa605bb38d04ecea9f344c1bdfea80fcb66502
+      </div>b66502
     <router-view/>
   </div>
 </template>
@@ -31,7 +22,7 @@ import PageAuthorization from '@/components/PageAuthorization.vue'
 export default {
 name: 'home',
     components: {
-        Header
+        Header, PageAuthorization
     }
 }
 </script>
@@ -49,15 +40,10 @@ name: 'home',
     align-items: center
     -webkit-font-smoothing: antialiased
     -moz-osx-font-smoothing: grayscale
-<<<<<<< HEAD
-<<<<<<< HEAD
     background: #fff url('assets/bg-image.png') center no-repeat
     font-family: 'Roboto'
 #page
     width: 1440px
-=======
-=======
->>>>>>> 08fa605bb38d04ecea9f344c1bdfea80fcb66502
     font-family: 'Roboto'
 .wrapper-page
     width: 1440px
@@ -65,10 +51,6 @@ name: 'home',
     background: #fff url('assets/bg-image.png') center no-repeat
 #page
     width: 100%
-<<<<<<< HEAD
->>>>>>> 08fa605bb38d04ecea9f344c1bdfea80fcb66502
-=======
->>>>>>> 08fa605bb38d04ecea9f344c1bdfea80fcb66502
     min-height: 100vh
     background: rgba(#502090, 0.5)
 </style>
