c0d71f98c8c6614892d822f4a91ee2a61bfd4de4
Uvv-v <Uvv-v@users.noreply.github.com>
2019-07-04 12:05:55 +0400

    change header

--- Front/project/src/App.vue
+++ Front/project/src/App.vue
@@ -1,42 +1,41 @@
 <template>
   <div id="app">
-    <div id="page">
-      <Header></Header>
-      <Footer></Footer>
+    <div id="page" class="backgrount_color__main">
+        <Header></Header>
+        <Footer></Footer>
     </div>
     <router-view/>
-    
   </div>
 </template>
-
-<style lang="css">
-*{
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-}
-#app{
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
-
-  font-family: 'Avenir', Halvetica, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-}
-#page{
-  width: 100vw;
-}
-</style>
 
 <script>
 import Header from '@/components/Header.vue'
 import Footer from '@/components/Footer.vue'
 export default {
-  name: 'home',
-  components: {
-    Header
-  }
+name: 'home',
+    components: {
+        Header
+    }
 }
 </script>
+
+<style lang="sass">
+@import '/assets/fonts/raleway/raleway.css'
+*
+    margin: 0
+    padding: 0
+    box-sizing: border-box
+#app
+    display: flex
+    flex-direction: row
+    justify-content: center
+    align-items: center
+    -webkit-font-smoothing: antialiased
+    -moz-osx-font-smoothing: grayscale
+    background: #fff url('assets/bg-image.png') center no-repeat
+    font-family: 'Roboto'
+#page
+    width: 1440px
+    min-height: 100vh
+    background: rgba(#502090, 0.5)
+</style>
--- Front/project/src/components/Header.vue
+++ Front/project/src/components/Header.vue
@@ -1,31 +1,78 @@
 <template>
     <div class="wrapper">
-        <div class="logo"></div>
-        <router-link to="/films" class="link">Фильмы</router-link>
-        <router-link to="/news" class="link">Новости</router-link>
-        <router-link to="/contacts" class="link">Контакты</router-link>
-        <button class="signIn">Войти</button>
+        <div style="flex: 1"></div>
+        <router-link to="/" class="logo"></router-link>
+        <div style="flex: 2"></div>
+        <div class="wrapper-links">
+            <router-link to="/films" class="link-button">
+                <p class="link-text">Фильмы</p>
+            </router-link>
+            <router-link to="/news" class="link-button">
+                <p class="link-text">Новости</p>
+            </router-link>
+            <router-link to="/contacts" class="link-button">
+                <p class="link-text">О нас</p>
+            </router-link>
+            <div class="link-button">
+                <p class="link-text">Еще</p>
+            </div>
+        </div>
+        <div class="signIn">
+            <div class="signIn-logo"></div>
+            <p class="link-text">Войти</p>
+        </div>
+        <div style="flex: 1"></div>
     </div>
 </template>
  
 <style lang="sass" scoped>
-$logo-width : 200px
-$logo-height : $logo-width
+$logo-width : 170.4px
+$logo-height : 71.2px
+$links_font-size: 24px
 .wrapper
-    background-color: #F73
-    padding: 70px
+    padding: 31px
     width: 100%
-    height: 240px
+    height: 132px
     display: flex
+    flex-direction: row
     justify-content: space-around
     align-items: center
     .logo
         width: $logo-width
         height: $logo-height
-        background-color: #F33
-    .link
+        background: url('../assets/logo.svg') center no-repeat
+    .link-text
         text-decoration: none
-        color: black
-        font-size: 24px
-        font-weight: 100
+        font-family: 'Roboto', sans-serif
+        font-size: 12px
+        line-height: 14px
+        color: white
+    .wrapper-links
+        display: flex
+        margin-right: 56px
+        .link-button
+            display: flex
+            justify-content: center
+            align-items: center
+            margin: 0 10px
+            width: 121px
+            height: 35px
+            border: 1px solid white
+            border-radius: 15px
+            text-decoration: none
+            user-select: none
+            &:hover
+                background-color: rgba(white, 0.3)
+    .signIn
+        display: flex
+        justify-content: center
+        align-items: center
+        cursor: pointer
+        .signIn-logo
+            width: 45px
+            height: 45px
+            background: url('../assets/signIn-logo.svg')
+        &:hover .link-text
+            text-decoration: underline
+    
 </style>
