fae795764fa7521779b196ba494174eed72fcef7
ArtemixPG <32619714+ArtemixPG@users.noreply.github.com>
2019-07-04 23:59:45 +0400

    add Controller and Filter Encoding with change Spring Security Configuration

---
+++ Back/CinemaTicketsRealase/src/main/java/com/controller/HelloController.java
@@ -0,0 +1,36 @@
+package com.controller;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+public class HelloController {
+private static String[] LISTFILMS = {"Мстители", "Железный человек", "Миссия невыполнима",
+"Марсианин", "Интерстеллар", "Принц Персии"};
+private List<HashData> list;
+    @GetMapping("/hello")
+    public List<HashData> getHello(){
+        list = new ArrayList<>(6);
+        for (int i = 0; i < 6; i++){
+            HashData data = new HashData(i ,LISTFILMS[i]);
+            list.add(data);
+        }
+        return list;
+    }
+    @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
+    static class HashData{
+        private String name;
+        private int id;
+        public HashData(int id, String name){
+            this.id = id;
+            this.name = name;
+        }
+
+    }
+}
