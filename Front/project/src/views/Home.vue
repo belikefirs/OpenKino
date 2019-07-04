c36ccbec183d7690362fd31ce932c30bedacf5c7
Uvv-v <Uvv-v@users.noreply.github.com>
2019-07-04 18:04:08 +0400

    lalalala

--- Front/project/src/views/Home.vue
+++ Front/project/src/views/Home.vue
@@ -1,15 +1,16 @@
 <template>
-    
+    <NowFilm></NowFilm>
 </template>
 
 <script>
-import NowInCinema from '@/components/HomeNowInCinema.vue'
+import Now from '@/components/HomeNow.vue'
 import Actions from '@/components/HomeActions.vue'
 import News from '@/components/HomeNews.vue'
 import Activity from '@/components/HomeActivity.vue'
+import NowFilm from '@/components/HomeNowFilm'
 export default {
     components: {
-        NowInCinema, Actions, News, Activity
+        Now, Actions, News, Activity, NowFilm
     }
 }
 </script>
