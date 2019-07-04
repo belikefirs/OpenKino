e80d2197c66c3d19a7c13d5642e4cb5b01b972b3
maximelnui <52440685+maximelnui@users.noreply.github.com>
2019-07-04 13:13:59 +0400

    norm
    norm

--- Front/project/src/App.vue
+++ Front/project/src/App.vue
@@ -1,16 +1,25 @@
 <template>
   <div id="app">
+<<<<<<< HEAD
+<<<<<<< HEAD
     <div id="page" class="backgrount_color__main">
         <Header></Header>
         <Footer></Footer>
         <PageAuthorization></PageAuthorization>
     </div>
+=======
+=======
+>>>>>>> 08fa605bb38d04ecea9f344c1bdfea80fcb66502
       <div class="wrapper-page">
         <div id="page">
             <Header></Header>
             <Footer></Footer>
         </div>
-      </div>b66502
+      </div>
+<<<<<<< HEAD
+>>>>>>> 08fa605bb38d04ecea9f344c1bdfea80fcb66502
+=======
+>>>>>>> 08fa605bb38d04ecea9f344c1bdfea80fcb66502
     <router-view/>
   </div>
 </template>
@@ -22,7 +31,7 @@ import PageAuthorization from '@/components/PageAuthorization.vue'
 export default {
 name: 'home',
     components: {
-        Header, PageAuthorization
+        Header
     }
 }
 </script>
