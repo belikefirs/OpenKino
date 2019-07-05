45381126daa6ca6e77a3833c7f5c4bb7479f4558
maximelnui <52440685+maximelnui@users.noreply.github.com>
2019-07-05 11:45:22 +0400

    ADD CONNECTION DB

--- Back/CinemaTicketsRealase/src/main/java/com/configuration/JPAConfiguration.java
+++ Back/CinemaTicketsRealase/src/main/java/com/configuration/JPAConfiguration.java
@@ -1,5 +1,6 @@
 package com.configuration;
 
+import org.h2.jdbcx.JdbcDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -13,6 +14,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
+import java.util.HashMap;
 
 @Configuration
 @EnableJpaRepositories
@@ -20,23 +22,42 @@ import javax.sql.DataSource;
 public class JPAConfiguration {
     @Bean
     public DataSource dataSource(){
-        EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
-        return builder.setType(EmbeddedDatabaseType.H2).build();
+        JdbcDataSource jdbcDataSource = new JdbcDataSource();
+        jdbcDataSource.setURL("jdbc:h2:C:\\Users\\user\\Documents\\GitHub\\OpenKino\\OpenKino_DB");
+        jdbcDataSource.setUser("admin");
+        jdbcDataSource.setPassword("admin");
+
+        return jdbcDataSource;
     }
     @Bean
     public LocalContainerEntityManagerFactoryBean entityManagerFactory(){
+
+        HashMap<String, String > hibernatePropertys = new HashMap<>();
+        hibernatePropertys.put("hibernate.hbm2ddl.auto","update");
+        hibernatePropertys.put("hibernate.connection.driver_class","org.h2.Driver");
+        hibernatePropertys.put("hibernate.dialect","org.hibernate.dialect.H2Dialect");
+        hibernatePropertys.put("hibernate.show_sql","true");
+        hibernatePropertys.put("hibernate.generate_statistics","true");
+        hibernatePropertys.put("hibernate.connection.charSet","UTF-8");
+        hibernatePropertys.put("hibernate.format_sql","true");
+        hibernatePropertys.put("hibernate.use_sql_comments","true");
+
         HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+
         adapter.setGenerateDdl(true);
         LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
         factory.setJpaVendorAdapter(adapter);
-        factory.setPackagesToScan("com.configuration");
+        factory.setPackagesToScan("com");
         factory.setDataSource(dataSource());
+
+        factory.setJpaPropertyMap(hibernatePropertys);
         return factory;
     }
     @Bean
     public PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory){
         JpaTransactionManager manager = new JpaTransactionManager();
         manager.setEntityManagerFactory(entityManagerFactory);
+        manager.setDataSource(dataSource());
         return manager;
     }
 }
