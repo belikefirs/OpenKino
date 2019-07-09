3e6eb92b68839771b7ed2069756d06b34aefcec7
Uvv-v <Uvv-v@users.noreply.github.com>
2019-07-10 00:36:55 +0400

    animation slider

--- Front/project/src/components/AppSlider.vue
+++ Front/project/src/components/AppSlider.vue
@@ -1,9 +1,8 @@
 <template>
     <div class="wrapper-slider" v-resize="onResize">
         <div class="previous" v-on:click="prevClick"></div>
-        <transition-group name="slider" class="slider">
-            <SliderItem
-            v-for="(item, name) in uData" :key="name"
+        <transition-group name="slider" tag="div">
+            <SliderItem v-for="item in uData" :key="item"
             :image="item.image"
             :path="item.path"
             :filmTitle="item.filmTitle"
@@ -27,17 +26,12 @@ export default {
             required: true
         }
     },
-    watch:{
-        'uIndex'(){},
-        'uData'(){}
-    },
     data () {
         let uData = [];
         this.pData.forEach(function(item, index){
             uData[index] = item;
         });
         return {
-            uIndex : 0,
             maxInPage : 1,
             uData
         }
@@ -47,12 +41,10 @@ export default {
             this.maxInPage = Math.round((document.documentElement.clientWidth - 220) / 248);
         },
         nextClick(){
-            //this.uIndex = (this.uIndex + 1) % this.uData.length;
-            this.uData.unshift(this.uData.pop());
+            this.uData.push(this.uData.shift());
         },
         prevClick(){
-            //this.uIndex = (this.uData.length + this.uIndex - 1) % this.uData.length;
-            this.uData.push(this.uData.shift());
+            this.uData.unshift(this.uData.pop());
         }
     },
     computed:{
@@ -62,13 +54,24 @@ export default {
 </script>
 
 <style lang="sass" scoped>
+// .slider-move
+//     transition: transform 1s
+// .slider-enter-active, .slider-leave-active 
+//     transition: all 1s
+// .slider-enter, .slider-leave-to
+//     opacity: 0
+//     transform: translateY(30px)
 .wrapper-slider
     position: relative
     width: 100%
     height: 466px
     background: rgba(#212121, 0.4)
-    display: flex
-    justify-content: center
+    &>div
+        width: 100%
+        height: 100%
+        display: flex
+        flex-direction: row
+        justify-content: center
     .previous, .next
         z-index: 1
         position: absolute
@@ -85,23 +88,24 @@ export default {
     .previous
         left: 35px
     .next
-        right: 35px
-    .slider
-        display: flex
-        flex-direction: row
+        right: 35px        
 </style>
 
-
-<style>
-.list-item {
+<style scoped>
+.slider-move{
+    transition: transform 1s;
+}
+.slider-complete-item {
+  transition: all 1s;
   display: inline-block;
   margin-right: 10px;
 }
-.list-enter-active, .list-leave-active {
-  transition: all 1s;
-}
-.list-enter, .list-leave-to /* .list-leave-active до версии 2.1.8 */ {
+.slider-complete-enter, .slider-complete-leave-to
+/* .list-complete-leave-active до версии 2.1.8 */ {
   opacity: 0;
   transform: translateY(30px);
+}
+.slider-complete-leave-active {
+  position: absolute;
 }
 </style>
