37618aa3a22c38155234b53e68d7fa9548fdfde9
maximelnui <52440685+maximelnui@users.noreply.github.com>
2019-07-18 12:46:20 +0400

    update FilmService

--- Back/CinemaTicketsRealase/src/main/java/com/controller/FilmController.java
+++ Back/CinemaTicketsRealase/src/main/java/com/controller/FilmController.java
@@ -18,45 +18,57 @@ public class FilmController {
     public FilmController(FilmService filmService) {
         this.filmService = filmService;
     }
-
+    ///=================================Film========================
     @PostMapping("")
     public Long saveFilm(@RequestBody Film film){
         return filmService.saveFilm(film);
     }
-
-    @PutMapping("/update")
+    @PutMapping("")
     public Long updateFilm(@RequestBody Film film){
         return filmService.updateFilmById(film);
     }
-
-    @DeleteMapping("/delete")
-
+    @DeleteMapping("/{id}")
+    public void deleteFilmById(@PathVariable Long id){
+    filmService.deleteFilmById(id);
+    }
     @GetMapping("/{id}")
     public Film get(@PathVariable Long id){
         return filmService.findFilmById(id);
     }
-    @GetMapping("/all")
+    @GetMapping("")
     public List <Film> getAll(){
         return filmService.findAllFilm();
     }
 
+    ///=================================TypeFilm========================
     @GetMapping("/type")
     public List<TypeFilm> getType(){
         return filmService.findAllTypeFilm();
     }
+    @PutMapping("/type")
+    public Long update(@RequestBody TypeFilm typeFilm){
+       return filmService.updateTypeFilm(typeFilm);
+    }
 
+    ///=================================Genre========================
     @GetMapping("/genre")
     public List<Genre> getGenre(){
         return filmService.findAllGenre();
     }
+    @PutMapping("/genre")
+    public Long update(@RequestBody Genre genre){
+        return filmService.updateGenre(genre);
+    }
 
+    ///=================================LimitAge========================
     @GetMapping("/limitAge")
     public List<LimitAge> getLimitage(){
         return filmService.findAllLimitAge();
     }
-
-
-
+    @PutMapping("/limitAge")
+    public Long updateLimitAge(LimitAge limitAge){
+        return filmService.updateLimitAge(limitAge);
+    }
 
 
 }
