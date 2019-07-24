dd4205fb67e3ba83162bbd56ab66371005e55160
maximelnui <52440685+maximelnui@users.noreply.github.com>
2019-07-24 08:57:18 +0400

    nothing

--- Front/project/src/views/News.vue
+++ Front/project/src/views/News.vue
@@ -1,21 +1,33 @@
 <template>
     <div>
+        <!--<div class="wrapper-NewsFilm">
+            <NewsFilm v-for="item in $store.state.newsFilm" :key="item.filmTitle "
+                :image="item.image"
+                :path="item.path"
+                :filmTitle="item.filmTitle"
+                :filmNews="item.filmNews"
+                :newTitle="item.newTitle"
+            >
+        </NewsFilm>
+        </div>-->
         <SelectionsForYou></SelectionsForYou>
-        <NewsFilm></NewsFilm>
     </div>
 </template>
-
 <script>
 import SelectionsForYou from '@/components/FilmsSelectionsForYou.vue'
 import NewsFilm from '@/components/NewsFilms.vue'
 
 export default {
     components: {
-        SelectionsForYou
+        SelectionsForYou, NewsFilm
     }
 }
 </script>
 
 <style>
-
+.wrapper-NewsFilm{
+    display: flex;
+    flex-wrap: wrap;
+    width: 100%;
+}
 </style>
