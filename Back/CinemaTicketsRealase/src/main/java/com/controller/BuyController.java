136d2ae03006aa38d40abef128ff9d13116b450c
mbelowwww <mbelowwww@yandex.ru>
2019-07-31 09:43:00 +0400

    creat BuyDao,BuyService,BuyController.

---
+++ Back/CinemaTicketsRealase/src/main/java/com/controller/BuyController.java
@@ -0,0 +1,26 @@
+package com.controller;
+
+import com.models.Buy;
+import com.service.BuyService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/buy")
+public class BuyController {
+
+    private BuyService buyService;
+
+    public BuyController(BuyService buyService) {
+        this.buyService = buyService;
+    }
+
+    @PostMapping("")
+    Long saveBuy(@RequestBody Buy buy){
+        return buyService.saveBuy(buy);
+    }
+
+    @GetMapping("/id/{id}")
+    Buy findById(@PathVariable Long id){
+        return buyService.findBuyById(id);
+    }
+}
