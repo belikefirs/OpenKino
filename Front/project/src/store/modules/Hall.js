288e6738b817544e00e3b0e2a0a0d1a5d94cf387
maximelnui <52440685+maximelnui@users.noreply.github.com>
2019-08-29 14:42:00 +0400

    prepare

--- Front/project/src/store/modules/Hall.js
+++ Front/project/src/store/modules/Hall.js
@@ -1,29 +1,33 @@
 import AXIOS from 'axios';
 
 const state = {
-    halldata: null
+    halls: []
 }
 
+const getters = {
+    HALLS: state => {
+    return state.halls;
+    }
+}
 const mutations = {
-    showHall(state, data) {
-        state.halldata = data
+    SET_LIST_HALLS: (state, data) => {
+        state.halls = data
     },
 }
 
 const actions = {
-    GET_HALL (context) {
-        return AXIOS.get('/hall/get/6').then(({data}) => {
-            return data;
-        })
-    },
-    CREATE_HALLS(context, data) {
-        return AXIOS.get('/hall/saveall').then(({data}) => {
-            return data;
-        })
+    // GET_HALL (context) {
+    //     return AXIOS.get('/hall/get/6').then(({data}) => {
+    //         return data;
+    //     })
+    // },
+    GET_LIST_HALLS: async (context, data) =>{
+        let {temp} = await AXIOS.get('/hall/all')
+            context.commit('SET_LIST_HALLS', temp)
     }
 }
 
 export default {
     namespaced: true,
-    state, mutations, actions
+    state, mutations, actions, getters
 }
